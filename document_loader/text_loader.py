7c3a2db4a71c62ed5270c3840fc1214a3241ab55
Sushil <chaubeyshushil000@gmail.com>
2025-12-18 23:15:45 +0530

    add document loader

---
+++ document_loader/text_loader.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
+
+loader=TextLoader('ai.txt',encoding='utf-8')
+
+docs=loader.load()
+
+prompt1=PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+parser=StrOutputParser()
+
+chain=prompt1 | model | parser
+
+print(docs)
+
+
